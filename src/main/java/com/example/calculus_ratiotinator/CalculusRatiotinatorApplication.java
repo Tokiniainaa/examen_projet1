21ffe7e3b58886366910286e3a13d68d23180579
tokiniaina <hei.tokiniaina@gmail.com>
2024-06-07 14:19:36 +0300

    chore: initialize project with java 21

---
+++ src/main/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculus_ratiotinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculusRatiotinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculusRatiotinatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculus_ratiotinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculusRatiotinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
