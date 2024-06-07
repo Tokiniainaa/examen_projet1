228360250372fe105bc1921c403a43ff48c6da44
tokiniaina <hei.tokiniaina@gmail.com>
2024-06-07 16:31:13 +0300

    test: add testMensonge

--- src/test/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplicationTests.java
+++ src/test/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.calculus_ratiotinator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -12,6 +13,11 @@ class CalculusRatiotinatorApplicationTests {
     public void testVerite() {
         InterfaceAffirmation affirmation = new Verite();
         assertTrue(affirmation.evaluer());
+    }
+    @Test
+    public void testMensonge() {
+        InterfaceAffirmation affirmation = new Mensonge();
+        assertFalse(affirmation.evaluer());
     }
 
 }
