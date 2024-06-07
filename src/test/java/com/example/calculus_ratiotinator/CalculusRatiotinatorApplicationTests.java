b7f3567bd3c599ea65a2ec9f0fb1296774d3d2c1
tokiniaina <hei.tokiniaina@gmail.com>
2024-06-07 16:36:47 +0300

    test: add test for all conjonction (ET,OU,DONC)

--- src/test/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplicationTests.java
+++ src/test/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.calculus_ratiotinator;
 
+import com.example.calculus_ratiotinator.conjonction.ConjonctionDonc;
+import com.example.calculus_ratiotinator.conjonction.ConjonctionEt;
+import com.example.calculus_ratiotinator.conjonction.ConjonctionOu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -22,6 +25,27 @@ class CalculusRatiotinatorApplicationTests {
     public void testAffirmation() {
         InterfaceAffirmation affirmation = new Affirmation(true);
         assertTrue(affirmation.evaluer());
+    }
+    @Test
+    public void testEt() {
+        InterfaceAffirmation affirmation1 = new Verite();
+        InterfaceAffirmation affirmation2 = new Mensonge();
+        InterfaceAffirmation conjonctionEt = new ConjonctionEt(affirmation1, affirmation2);
+        assertFalse(conjonctionEt.evaluer());
+    }
+    @Test
+    public void testOu() {
+        InterfaceAffirmation affirmation1 = new Verite();
+        InterfaceAffirmation affirmation2 = new Mensonge();
+        InterfaceAffirmation conjonctionOu = new ConjonctionOu(affirmation1, affirmation2);
+        assertTrue(conjonctionOu.evaluer());
+    }
+    @Test
+    public void testDonc() {
+        InterfaceAffirmation affirmation1 = new Verite();
+        InterfaceAffirmation affirmation2 = new Mensonge();
+        InterfaceAffirmation conjonctionDonc = new ConjonctionDonc(affirmation1, affirmation2);
+        assertFalse(conjonctionDonc.evaluer());
     }
 
 }
