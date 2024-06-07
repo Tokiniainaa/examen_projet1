23b57a4447acfdbc8ce66fb9f3ebd61ede0da7ac
tokiniaina <hei.tokiniaina@gmail.com>
2024-06-07 16:29:03 +0300

    test: add testVerite

--- src/test/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplicationTests.java
+++ src/test/java/com/example/calculus_ratiotinator/CalculusRatiotinatorApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.calculus_ratiotinator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class CalculusRatiotinatorApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testVerite() {
+        InterfaceAffirmation affirmation = new Verite();
+        assertTrue(affirmation.evaluer());
     }
 
 }
