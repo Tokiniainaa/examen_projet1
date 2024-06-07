67641418d79c385e1eee7a47a8bc2b73f488cb86
tokiniaina <hei.tokiniaina@gmail.com>
2024-06-07 16:19:32 +0300

    refactor: IntrefaceAffirmation -> InterfaceAffirmation

--- src/main/java/com/example/calculus_ratiotinator/Affirmation.java
+++ src/main/java/com/example/calculus_ratiotinator/Affirmation.java
@@ -3,7 +3,7 @@ package com.example.calculus_ratiotinator;
 import lombok.Setter;
 
 @Setter
-public class Affirmation implements IntrefaceAffirmation{
+public class Affirmation implements InterfaceAffirmation {
     private final boolean condition;
     public Affirmation(boolean condition) {
         this.condition = condition;
--- src/main/java/com/example/calculus_ratiotinator/InterfaceAffirmation.java
+++ src/main/java/com/example/calculus_ratiotinator/InterfaceAffirmation.java
@@ -1,6 +1,6 @@
 package com.example.calculus_ratiotinator;
 
-public interface IntrefaceAffirmation {
+public interface InterfaceAffirmation {
     boolean evaluer();
 
 }
