8a768dc6f11aa54305a986639e1d9ce608fe960a
tokiniaina <hei.tokiniaina@gmail.com>
2024-06-07 16:16:34 +0300

    feat: implement InterfaceAffirmation in Mensonge and Verite

---
+++ src/main/java/com/example/calculus_ratiotinator/Mensonge.java
@@ -0,0 +1,8 @@
+package com.example.calculus_ratiotinator;
+
+public final class Mensonge implements InterfaceAffirmation {
+    @Override
+    public boolean evaluer() {
+        return false;
+    }
+}
---
+++ src/main/java/com/example/calculus_ratiotinator/Verite.java
@@ -0,0 +1,9 @@
+package com.example.calculus_ratiotinator;
+
+public final class Verite implements InterfaceAffirmation {
+
+    @Override
+    public boolean evaluer() {
+        return false;
+    }
+}
